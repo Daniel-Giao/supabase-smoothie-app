74e06377f18d15805379974f1788572109cf10ca
Daniel Giao <daniel.giao@icloud.com>
2024-08-05 00:53:35 -0400

    Connecting React app to Supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
+// config import
+import supabase from "../config/supabaseClient"
+
 const Home = () => {
+  console.log(supabase);
+  
   return (
     <div className="page home">
       <h2>Home</h2>
