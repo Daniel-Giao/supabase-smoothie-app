1465b0a3c268e460c070a3bc78cd9d89f6e9179a
Daniel Giao <daniel.giao@icloud.com>
2024-08-05 01:15:59 -0400

    Fetching data from supabase table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,49 @@
+import { useEffect, useState } from "react"
+
 // config import
 import supabase from "../config/supabaseClient"
 
+// components import
+import SmoothieCard from "../components/SmoothieCard"
+
 const Home = () => {
-  console.log(supabase);
-  
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+
+      if (error) {
+        setFetchError('Could not fetch the smoothies')
+        setSmoothies(null)
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (
+        <p>{fetchError}</p>
+      )}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
